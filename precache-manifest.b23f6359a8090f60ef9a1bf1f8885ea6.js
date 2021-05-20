2e81adb152cd9563bf1cb2eb4c2d58df2533b3aa
Dima Batiuk <batiuk20@gmail.com>
2021-05-20 12:53:19 +0300

    Updates

---
+++ precache-manifest.b23f6359a8090f60ef9a1bf1f8885ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4679b46b79e3f0eeeb01893016887140",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0651befd6605ea26c7d0",
+    "url": "./static/css/main.a3cc3a90.chunk.css"
+  },
+  {
+    "revision": "486876fe5217784983bf",
+    "url": "./static/js/2.79d301ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.79d301ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0651befd6605ea26c7d0",
+    "url": "./static/js/main.2cdb880f.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
